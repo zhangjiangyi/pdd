fc4768cba73f21629e5b33f351aebec124ce80da
DESKTOP-2S174DK\4399 <zhangjiangyi@4399inc.com>
2019-12-16 20:06:06 +0800

    增加ua管理

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 process.on('uncaughtException', function (err) { console.log(err) });
 const Koa = require('koa');
 const app = new Koa();
-const db = require('better-sqlite3')('config.db');
+const userDB = require('better-sqlite3')('config.db');
+const uaDB = require('better-sqlite3')('ua.db');
 const devices = require('puppeteer/DeviceDescriptors');
 const puppeteer = require('puppeteer');
-process.on('exit', () => db.close());
+const ipControl = require('./ipcontrol');
+process.on('exit', () => userDB.close() && uaDB.close());
 process.on('SIGHUP', () => process.exit(128 + 1));
 process.on('SIGINT', () => process.exit(128 + 2));
 process.on('SIGTERM', () => process.exit(128 + 15));
@@ -15,7 +17,8 @@ function rand(min, max) {
 	return Math.floor(Math.random() * (max - min)) + min;
 }
 
-db.prepare('create table if not exists tokens (token text unique, used numeric)').run();
+userDB.prepare('create table if not exists tokens (token text unique, used numeric)').run();
+uaDB.prepare('create table if not exists ua (ua text unique, times numeric)').run();
 
 async function waitForClose(page) {
 	return new Promise((resolve, reject) => {
@@ -23,8 +26,13 @@ async function waitForClose(page) {
 	});
 }
 
-async function spider(token) {
-	var args = ['--no-sandbox'];
+async function spider(user) {
+	let ip = await controlIp(user);
+	let ua = getUA();
+	var args = [
+		'--no-sandbox'
+		, `--proxy-server=${ip}`		// 设置代理
+	];
 	var browser;
 	try {
 		browser = await puppeteer.launch({
@@ -33,21 +41,41 @@ async function spider(token) {
 			args: args
 		});
 		const page = await browser.newPage();
+		addInterception(page, browser);
 		await page.emulate(devices['iPhone 7']);
-		await page.setUserAgent('alipay'); // UA填这里面
+		await page.setUserAgent(ua); // UA填这里面
 		await page.goto('https://mobile.yangkeduo.com/');
-		await page.setCookie({ name: 'PDDAccessToken', value: token });
+		await page.setCookie({ name: 'PDDAccessToken', value: user.token });
 		await waitForClose(page);
 	} catch (e) {
 		console.log('浏览器运行错误！');
 	}
 }
 
+async function controlIp(user) {
+	let ips = await ipControl.getProxyList();
+	return ips[0]
+}
+
+function getUA() {
+	return uaDB.prepare('SELECT * FROM ua ORDER BY RANDOM() limit 1').get();
+}
+
+function addInterception(page, browser) {
+	// 启用请求拦截
+	page.setRequestInterception(true);
+	//监听请求事件
+	page.on('request', (req) => {
+		// console.log('拦截的headers-------------------------------------' + JSON.stringify(req.headers()));
+		req.continue();
+	})
+}
+
 function getToken() {
-	var token = db.prepare('select token from tokens where used = 0').get();
+	var token = userDB.prepare('select token from tokens where used = 0').get();
 	if (token) {
 		token = token.token;
-		db.prepare('update tokens set used = 1 where token = ?').run(token);
+		userDB.prepare('update tokens set used = 1 where token = ?').run(token);
 	}
 	return token;
 }
@@ -66,13 +94,13 @@ async function startBrowser() {
 
 app.use(require('koa-static')(__dirname + '/files'));
 
-app.use((ctx) => {
+app.use(async (ctx) => {
 	switch (ctx.path) {
 		case '/delToken':
 			var tokens = ctx.query.tokens;
 			if (tokens) {
 				tokens = JSON.parse(tokens);
-				const stmtd = db.prepare('delete from tokens where token = ?');
+				const stmtd = userDB.prepare('delete from tokens where token = ?');
 				for (var i = 0; i < tokens.length; i++)
 					stmtd.run(tokens[i]);
 				ctx.body = 'ok';
@@ -82,26 +110,56 @@ app.use((ctx) => {
 			var tokens = ctx.query.tokens;
 			if (tokens) {
 				tokens = JSON.parse(tokens);
-				const stmti = db.prepare('insert or replace into tokens (token, used) values (?, 0)');
+				const stmti = userDB.prepare('insert or replace into tokens (token, used) values (?, 0)');
 				for (var i = 0; i < tokens.length; i++) {
 					if (tokens[i]) {
 						stmti.run(tokens[i]);
-						if (!running) {
-							running = true;
-							startBrowser();
-						}
 					}
 				}
 				ctx.body = 'ok';
 			} else ctx.body = 'no';
 			break;
+		case '/openPage':
+			var users = JSON.parse(ctx.query.users || []);
+			if (users) {
+				users.forEach(user => spider(user));
+			}
+			ctx.body = 'ok';
+			break;
 		case '/showToken':
-			ctx.body = db.prepare('select * from tokens').all();
+			ctx.body = userDB.prepare('select * from tokens').all();
+			break;
+
+		// ua处理 最好跟token一起搞成一个通用组件（增删改查）
+		case '/showUA':
+			ctx.body = uaDB.prepare('select * from ua').all();
+			break;
+		case '/addUA':
+			var uas = ctx.query.uas;
+			uas = JSON.parse(uas);
+			const stmti = uaDB.prepare('insert or replace into ua (ua) values (?)');
+			for (var i = 0; i < uas.length; i++) {
+				if (uas[i]) {
+					stmti.run(uas[i]);
+				}
+			}
+			ctx.body = 'ok';
+			break;
+		case '/delUA':
+			var uas = ctx.query.uas;
+			if (uas) {
+				uas = JSON.parse(uas);
+				const stmtd = uaDB.prepare('delete from ua where ua = ?');
+				for (var i = 0; i < uas.length; i++)
+					stmtd.run(uas[i]);
+				ctx.body = 'ok';
+			} else ctx.body = 'no';
+			break;
 			break;
 	}
 });
 
 app.listen(8888, () => {
 	console.log('服务已启动。');
-	startBrowser();
+	// startBrowser();
 });
---
+++ ipcontrol.js
@@ -0,0 +1,43 @@
+var request = require("request");
+// var iconv = require('iconv-lite');
+var Promise = require("bluebird");
+
+function getProxyList() {
+    let num = 1;        // 提取数量
+    let type = 2;       // 数据格式：1:TXT 2:JSON 3:html
+    let yys = 0;        // 0:不限 100026:联通 100017:电信
+    let port = 11;      // IP协议 1:HTTP 2:SOCK5 11:HTTPS
+    let ts = 1;	        // 是否显示IP过期时间: 1显示 2不显示
+    let mr = 1;	        // 去重选择（1:360天去重 2:单日去重 3:不去重）
+    let apiURL = `http://http.tiqu.alicdns.com/getip3?num=${num}&type=${type}&yys=${yys}&port=${port}&pack=77211&ts=${ts}&mr=${mr}`;
+
+    return new Promise((resolve, reject) => {
+        var options = {
+            method: 'GET',
+            url: apiURL
+            // gzip: true,
+            // encoding: null,
+            // headers: {
+            //     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+            //     'Accept-Encoding': 'gzip, deflate',
+            //     'Accept-Language': 'zh-CN,zh;q=0.8,en;q=0.6,zh-TW;q=0.4',
+            //     'User-Agent': 'Mozilla/8.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.101 Safari/537.36'
+            // },
+        };
+
+        request(options, function (error, response, body) {
+            try {
+                if (error) throw error;
+                let bodyObj = JSON.parse(body);
+                let ips = bodyObj.data.map(data => data.ip + ':' + data.port);
+                resolve(ips);
+            } catch (e) {
+                return reject(e);
+            }
+        });
+    })
+}
+
+module.exports = {
+    getProxyList
+}
