7dbe0dff60a30f2e397dcf01863c4189ac3a5c74
DESKTOP-2S174DK\4399 <zhangjiangyi@4399inc.com>
2019-12-16 20:11:02 +0800

    套餐id

--- ipcontrol.js
+++ ipcontrol.js
@@ -4,12 +4,13 @@ var Promise = require("bluebird");
 
 function getProxyList() {
     let num = 1;        // 提取数量
+    let pack = 77211;   // 套餐id
     let type = 2;       // 数据格式：1:TXT 2:JSON 3:html
     let yys = 0;        // 0:不限 100026:联通 100017:电信
     let port = 11;      // IP协议 1:HTTP 2:SOCK5 11:HTTPS
     let ts = 1;	        // 是否显示IP过期时间: 1显示 2不显示
     let mr = 1;	        // 去重选择（1:360天去重 2:单日去重 3:不去重）
-    let apiURL = `http://http.tiqu.alicdns.com/getip3?num=${num}&type=${type}&yys=${yys}&port=${port}&pack=77211&ts=${ts}&mr=${mr}`;
+    let apiURL = `http://http.tiqu.alicdns.com/getip3?num=${num}&type=${type}&yys=${yys}&port=${port}&pack=${pack}&ts=${ts}&mr=${mr}`;
 
     return new Promise((resolve, reject) => {
         var options = {
